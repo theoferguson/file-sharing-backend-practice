8950d843ba37bbf00a72bbca1e92d49b3d382bde
Theo Ferguson <ferguson.theo@gmail.com>
2022-11-08 11:28:06 -0500

    finishing practice project

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # file-sharing-backend-practice
+
+Simple file sharing site in order to practice using Express and MongoDB/Mongoose. Password protection and download count included.
--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const bcrypt = require('bcrypt')
 const File = require('./models/File')
 
 const express = require('express')
-const req = require('express/lib/request')
 const app = express()
+app.use(express.urlencoded({ extended: true }))
 
 const upload = multer({ dest: 'uploads' })
 
@@ -32,8 +32,28 @@ app.post('/upload', upload.single('file'), async (req, res) => {
     res.render("index", { fileLink: `${req.headers.origin}/file/${file.id}` })
 })
 
-app.get("/file/:id", (req, res) => {
+app.route("/file/:id").get(handleDownload).post(handleDownload)
 
-})
+async function handleDownload(req, res) {
+    const file = await File.findById(req.params.id)
 
-app.listen(3000)
+    if (file.password != null) {
+        if (req.body.password == null) {
+            res.render("password")
+            return
+        }
+
+        if (!(await bcrypt.compare(req.body.password, file.password))) {
+            res.render("password", { error: true })
+            return
+        }
+    }
+
+    file.downloadCount++
+    await file.save()
+    console.log(file.downloadCount)
+
+    res.download(file.path, file.originalName)
+}
+
+app.listen(process.env.PORT)
