c3c4805970deb36610dfd5c380c8e66beeb19d46
Theo Ferguson <ferguson.theo@gmail.com>
2022-11-08 11:05:08 -0500

    init and early file sharing server logic

---
+++ server.js
@@ -0,0 +1,39 @@
+require("dotenv").config()
+const multer = require('multer')
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const File = require('./models/File')
+
+const express = require('express')
+const req = require('express/lib/request')
+const app = express()
+
+const upload = multer({ dest: 'uploads' })
+
+mongoose.connect(process.env.DATABASE_URL)
+
+app.set("view engine", "ejs")
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.post('/upload', upload.single('file'), async (req, res) => {
+    const fileData = {
+        path: req.file.path,
+        originalName: req.file.originalname
+    }
+    if (req.body.password != null && req.body.password !== '') {
+        fileData.password = await bcrypt.hash(req.body.password, 10)
+    }
+
+    const file = await File.create(fileData)
+
+    res.render("index", { fileLink: `${req.headers.origin}/file/${file.id}` })
+})
+
+app.get("/file/:id", (req, res) => {
+
+})
+
+app.listen(3000)
